cdfad209f5c9bd01c44c85e5d46d8a4037792b18
Robert <u1501851@rts-365.dk>
2021-06-09 11:08:32 +0200

    Removed submit function which is not in use anymore

--- js/fetch-animals.js
+++ js/fetch-animals.js
@@ -1,4 +1,3 @@
-console.log('script loaded');
 
 // ==== FETCH ====
 export async function fetchMyApi(myApiCollection) {
@@ -6,23 +5,3 @@ export async function fetchMyApi(myApiCollection) {
     const res = await req.json()
     return res
 }
-
-// ==== FORM ====
-const form = document.querySelector("#createAnimal");
-
-let submitAnimal = e => {
-    console.log("submit animal");
-    e.preventDefault()
-
-    const myFormData = new FormData();
-    myFormData.append("type", e.target.type.value)
-    myFormData.append("breed", e.target.breed.value)
-    myFormData.append("name", e.target.name.value)
-    myFormData.append("age", e.target.age.value)
-    myFormData.append("sex", e.target.sex.value)
-    myFormData.append("colors", e.target.colors.value)
-
-    console.log(myFormData);
-}
-
-if (form) form.addEventListener("submit", submitAnimal)
