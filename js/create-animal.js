a99e9a8fdcae5255c30ae00aaf96dfc80e3f3901
Robert <robertwattboolsen@gmail.com>
2021-06-09 18:20:20 +0200

    Rewrote postAnimal to be an async with await and try/catch

--- js/create-animal.js
+++ js/create-animal.js
@@ -1,4 +1,4 @@
-const saveBtn = document.querySelector('#saveAnimal');
+
 const form = document.querySelector('.form-create-animal');
 
 if (form) form.addEventListener("submit", e => {
@@ -6,20 +6,22 @@ if (form) form.addEventListener("submit", e => {
     postAnimal()
 })
 
-function postAnimal() {
-
-    fetch(`http://rowabo-myapi.herokuapp.com/api/v1/animals`, {
-        "method": "POST",
-        "headers": {
-            "Authorization": "Bearer asdjhfak354723ghwf2bf"
-        },
-        "body": new FormData(form)
-    })
-        .then(async ress => {
-            console.log(ress);
-            const result = await ress.json()
-            document.querySelector("main").innerHTML = `
-                <h1 style="text-align:center;margin-top:3rem;">${result.name} the ${result.type} has been saved!</h1>`
+async function postAnimal() {
+    try {
+        const req = await fetch(`http://rowabo-myapi.herokuapp.com/api/v1/animals`, {
+            "method": "POST",
+            "headers": {
+                "Authorization": "Bearer asdjhfak354723ghwf2bf"
+            },
+            "body": new FormData(form)
         })
-        .catch(err => console.error(err))
+
+        const res = await req.json()
+
+        document.querySelector("main").innerHTML = `
+            <h1 style="text-align:center;margin-top:3rem;">${res.name} the ${res.type} has been saved!</h1>`
+
+    } catch (err) {
+        console.error(err)
+    }
 }
