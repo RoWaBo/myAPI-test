21354dbcc0f7e15e5dde936abe21c84b845c079b
Robert <u1501851@rts-365.dk>
2021-06-09 08:57:57 +0200

    post new animal updated to use new formData

--- js/view-single-animal.js
+++ js/view-single-animal.js
@@ -4,12 +4,12 @@ import { addInfoBox } from "./info-box.js"
 const heading = document.querySelector(".animal-details__heading") 
 const table = document.querySelector(".valuepair-table")
 const animalID = new URLSearchParams(window.location.search).get("id")
-const enableEdit = new URLSearchParams(window.location.search).get("edit")
+const canEdit = new URLSearchParams(window.location.search).get("edit")
 
 init()
 async function init() {
     await printSingleAnimal()
-    if (enableEdit === "true") importInputsToTable()    
+    if (canEdit === "true") importInputsToTable()    
 }
 
 async function printSingleAnimal() {
@@ -89,13 +89,15 @@ function addBtns(headingInput) {
         <button id="cancel">CANCEL</button>
     `
     document.querySelector("main").append(div)
-    document.querySelector("#saveAnimal").addEventListener("click", () => createAnimal(headingInput))
-    document.querySelector("#cancel").addEventListener("click", () => {
-        console.log('cancel triggered');
-        const urlParams = new URLSearchParams(window.location.search)
+    document.querySelector("#saveAnimal").addEventListener("click", () => { createAnimal(headingInput), disableEditing() })
+    document.querySelector("#cancel").addEventListener("click", () => { disableEditing() })
+}
+function disableEditing() {
+    const urlParams = new URLSearchParams(window.location.search)
+    if (urlParams) {
         urlParams.delete('edit')
         window.location.search = urlParams    
-    })
+    }  
 }
 
 function patchAnimal(animalObject) {
